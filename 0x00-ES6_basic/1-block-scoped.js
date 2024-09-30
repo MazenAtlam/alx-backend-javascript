f729b8a1d4f9c9f4de2fe8ba07167709487ebef5
MazenAtlam <mazenatef5510@gmail.com>
2024-10-01 02:51:17 +0300

    fix task 1 v1.7

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -3,9 +3,10 @@ export default function taskBlock(trueOrFalse) {
   const task2 = true;
 
   if (trueOrFalse) {
-    const task = true;  // eslint-disable-next-line no-unused-vars, no-shadow
     // eslint-disable-next-line no-unused-vars, no-shadow
-    const task2 = false;
+    let task = true;
+    // eslint-disable-next-line no-unused-vars, no-shadow
+    let task2 = false;
   }
 
   return [task, task2];
