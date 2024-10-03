ddd7e1260c6c38d06a4f138a79852942a2aac5b0
MazenAtlam <mazenatef5510@gmail.com>
2024-10-04 01:39:21 +0300

    fix task 1 v1.1

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,8 +1,9 @@
-import ClassRoom from "./0-classroom.js";
+import ClassRoom from './0-classroom';
 
-function initializeRooms() {
+export default function initializeRooms() {
   const classRoomA = new ClassRoom(19);
   const classRoomB = new ClassRoom(20);
   const classRoomC = new ClassRoom(34);
+
   return [classRoomA, classRoomB, classRoomC];
 }
