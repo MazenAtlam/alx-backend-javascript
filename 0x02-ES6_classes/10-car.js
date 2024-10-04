02d0717daee5808ec3fa230a0687e3d59366ff91
MazenAtlam <mazenatef5510@gmail.com>
2024-10-04 22:32:33 +0300

    resolve cloneCar method-task 10 v1.1

--- 0x02-ES6_classes/10-car.js
+++ 0x02-ES6_classes/10-car.js
@@ -1,15 +1,15 @@
-const _brand = Symbol('brand');
-const _motor = Symbol('motor');
-const _color = Symbol('color');
-
 export default class Car {
   constructor(brand, motor, color) {
-    this[_brand] = brand;
-    this[_motor] = motor;
-    this[_color] = color;
+    this._brand = brand;
+    this._motor = motor;
+    this._color = color;
+  }
+
+  static get [Symbol.species]() {
+    return this;
   }
 
   cloneCar() {
-    return new this.constructor(this._brand, this._motor, this._color);
+    return new this.constructor[Symbol.species]();
   }
 }
