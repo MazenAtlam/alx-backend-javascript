9c2033630407a7f42dab7037b98ceb8485212841
MazenAtlam <mazenatef5510@gmail.com>
2024-10-04 17:31:50 +0300

    solve task 4

---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,34 @@
+/* eslint-disable no-underscore-dangle */
+
+import Currency from './3-currency';
+
+export default class Pricing {
+  constructor(amount, currency) {
+    this.amount = amount;
+    this.currency = currency;
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  set amount(value) {
+    this._amount = value;
+  }
+
+  set currency(value) {
+    this._currency = value;
+  }
+
+  displayFullPrice() {
+    return `${this.amount} ${this.currency.name} (${this.currency.code})`;
+  }
+
+  static convertPrice(amount, conversionRate) {
+    return amount * conversionRate;
+  }
+}
