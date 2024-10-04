f6b25ce52b1bd77b28af60e08ec414151428482e
MazenAtlam <mazenatef5510@gmail.com>
2024-10-04 20:54:17 +0300

    resolve holbertonClass and fullName getter in StudentHolbertonclass-task 9 v1.5

--- 0x02-ES6_classes/9-hoisting.js
+++ 0x02-ES6_classes/9-hoisting.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-underscore-dangle, max-classes-per-file */
 
-export class HolbertonClass {
+class HolbertonClass {
   constructor(year, location) {
     this._year = year;
     this._location = location;
@@ -15,28 +15,28 @@ export class HolbertonClass {
   }
 }
 
-const class2019 = new HolbertonClass(2019, 'San Francisco');
-const class2020 = new HolbertonClass(2020, 'San Francisco');
-
-export class StudentHolberton {
+class StudentHolberton {
   constructor(firstName, lastName, holbertonClass) {
     this._firstName = firstName;
     this._lastName = lastName;
     this._holbertonClass = holbertonClass;
   }
-
+  
   get fullName() {
     return `${this._firstName} ${this._lastName}`;
   }
-
+  
   get holbertonClass() {
-    return this.holbertonClass;
+    return this._holbertonClass;
   }
-
+  
   get fullStudentDescription() {
-    return `${this._firstName} ${this._lastName} - ${this._holbertonClass.year} - ${this._holbertonClass.location}`;
+    return `${this.fullName} - ${this.holbertonClass.year} - ${this.holbertonClass.location}`;
   }
 }
+
+const class2019 = new HolbertonClass(2019, 'San Francisco');
+const class2020 = new HolbertonClass(2020, 'San Francisco');
 
 const student1 = new StudentHolberton('Guillaume', 'Salva', class2020);
 const student2 = new StudentHolberton('John', 'Doe', class2020);
@@ -47,3 +47,4 @@ const student5 = new StudentHolberton('Jason', 'Sandler', class2019);
 const listOfStudents = [student1, student2, student3, student4, student5];
 
 export default listOfStudents;
+export { HolbertonClass, StudentHolberton };
