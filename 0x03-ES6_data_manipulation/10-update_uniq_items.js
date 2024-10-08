86b8df394f2032802d0cb17cf9a72ae0067424bf
MazenAtlam <mazenatef5510@gmial.com>
2024-10-08 20:00:58 +0300

    fix for ... of loop syntax-task 10 v1.1

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -3,7 +3,7 @@ export default function updateUniqueItems(map) {
     throw new Error('Cannot process');
   }
 
-  for (const {key, value} of map.entries()) {
+  for (const [key, value] of map.entries()) {
     if (value === 1) {
       map.set(key, 100);
     }
