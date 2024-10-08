feebf0ccd26099614d56dd24aade323deeacceb3
MazenAtlam <mazenatef5510@gmial.com>
2024-10-08 14:11:00 +0300

    eslint problem resolve-task 4 v1.4

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,15 +1,17 @@
+function getGrade(student, grades) {
+  const studentFilteredGrade = grades.filter((grade) => grade.studentId === student.id);
+  const grade = studentFilteredGrade.length === 0 ? 'N/A' : studentFilteredGrade[0].grade;
+
+  return grade;
+}
+
 export default function updateStudentGradeByCity(students, city, newGrades) {
   const results = students
     .filter((student) => student.location === city)
-    .map((student) => addGrade(student, newGrades));
+    .map((student) => {
+      Object.defineProperty(student, 'grade', getGrade(student, newGrades));
+      return student;
+    });
 
     return results;
-}
-
-function addGrade(student, grades) {
-  const studentFilteredGrade = grades.filter((grade) => grade.studentId === student.id);
-  let grade = studentFilteredGrade.length === 0 ? 'N/A' : studentFilteredGrade[0].grade;
-
-  student.grade = grade;
-  return student;
 }
